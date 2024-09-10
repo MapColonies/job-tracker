ca936bce64fdeb9ac61367765f062138a418dabc
nitzanmorr <nitzan.morr@gmail.com>
2024-09-10 15:24:39 +0300

    refactor: changed request sender function to match controller

--- tests/integration/tasks/helpers/requestSender.ts
+++ tests/integration/tasks/helpers/requestSender.ts
@@ -3,7 +3,7 @@ import * as supertest from 'supertest';
 export class TasksRequestSender {
   public constructor(private readonly app: Express.Application) {}
 
-  public async notifyTaskFinished(taskId: string): Promise<supertest.Response> {
+  public async handleTaskNotification(taskId: string): Promise<supertest.Response> {
     return supertest.agent(this.app).post(`/tasks/${taskId}/notify`).set('Content-Type', 'application/json');
   }
 }
