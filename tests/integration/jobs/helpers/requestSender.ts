bfac1beeb58f20c94bca3932b828feef82be8c1a
nitzanmorr <nitzan.morr@gmail.com>
2024-09-10 10:13:19 +0300

    test: modified supertest request sender for integration tests

--- tests/integration/jobs/helpers/requestSender.ts
+++ tests/integration/jobs/helpers/requestSender.ts
@@ -3,11 +3,7 @@ import * as supertest from 'supertest';
 export class JobsRequestSender {
   public constructor(private readonly app: Express.Application) {}
 
-  public async getResource(): Promise<supertest.Response> {
-    return supertest.agent(this.app).get('/jobs').set('Content-Type', 'application/json');
-  }
-
-  public async createResource(): Promise<supertest.Response> {
-    return supertest.agent(this.app).post('/jobs').set('Content-Type', 'application/json');
+  public async notifyFinished(jobId: string, taskId: string): Promise<supertest.Response> {
+    return supertest.agent(this.app).post(`/jobs/${jobId}/${taskId}/notify`).set('Content-Type', 'application/json');
   }
 }
