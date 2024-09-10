ef18077e710f8a77fd1b060eff7838caf9ec538b
nitzanmorr <nitzan.morr@gmail.com>
2024-09-10 10:39:26 +0300

    fix: lint

--- tests/integration/jobs/jobs.spec.ts
+++ tests/integration/jobs/jobs.spec.ts
@@ -1,6 +1,5 @@
 import jsLogger from '@map-colonies/js-logger';
 import { trace } from '@opentelemetry/api';
-import httpStatusCodes from 'http-status-codes';
 
 import { getApp } from '../../../src/app';
 import { SERVICES } from '../../../src/common/constants';
