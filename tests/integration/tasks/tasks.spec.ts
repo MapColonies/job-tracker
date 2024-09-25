bbcb0a2059a06a3dbf5ad0d71007e8215d789a29
nitzanmorr <nitzan.morr@gmail.com>
2024-09-25 10:57:16 +0300

    test: added more integration tests

--- tests/integration/tasks/tasks.spec.ts
+++ tests/integration/tasks/tasks.spec.ts
@@ -1,12 +1,15 @@
 import nock from 'nock';
 import { OperationStatus } from '@map-colonies/mc-priority-queue';
 import _ from 'lodash';
+import jestOpenAPI from 'jest-openapi';
 import { configMock } from '../../mocks/configMock';
 import { getApp } from '../../../src/app';
 import { IJobManagerConfig, ITaskTypesConfig } from '../../../src/common/interfaces';
 import { getIngestionJobMock, getTaskMock } from '../../mocks/JobMocks';
 import { TasksRequestSender } from './helpers/requestSender';
 import { getTestContainerConfig, resetContainer } from './helpers/containerConfig';
+
+// jestOpenAPI('openapi3.yaml');
 
 describe('tasks', function () {
   let requestSender: TasksRequestSender;
@@ -37,16 +40,17 @@ describe('tasks', function () {
       }
       nock.cleanAll();
     });
+
     it('Should return 200 and create polygon parts task when getting tiles merging completed task', async () => {
       // mocks
       const mockIngestionJob = getIngestionJobMock();
       const mockMergeTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.tilesMerging, OperationStatus.COMPLETED);
       const mockInitTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.init, OperationStatus.COMPLETED);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).post('/tasks/find', { id: mockMergeTask.id }).reply(200, [mockMergeTask]);
       nock(jobManagerConfigMock.jobManagerBaseUrl)
         .get(`/jobs/${mockIngestionJob.id}`)
         .query({ shouldReturnTasks: false })
         .reply(200, mockIngestionJob);
-      nock(jobManagerConfigMock.jobManagerBaseUrl).post('/tasks/find', { id: mockMergeTask.id }).reply(200, [mockMergeTask]);
       nock(jobManagerConfigMock.jobManagerBaseUrl)
         .post('/tasks/find', { jobId: mockIngestionJob.id, type: taskTypesConfigMock.init })
         .reply(200, [mockInitTask]);
@@ -57,16 +61,99 @@ describe('tasks', function () {
         .post(`/jobs/${mockIngestionJob.id}/tasks`, _.matches({ type: taskTypesConfigMock.polygonParts }))
         .reply(201);
       nock(jobManagerConfigMock.jobManagerBaseUrl).put(`/jobs/${mockIngestionJob.id}`).reply(200);
-      //action
+      // action
       const response = await requestSender.handleTaskNotification(mockMergeTask.id);
-      // assertion
+      // expectation
       expect(response.status).toBe(200);
+      expect(response).toSatisfyApiSpec();
+    });
+
+    it('Should return 200 and create finalize task when getting polygon parts completed task', async () => {
+      // mocks
+      const mockIngestionJob = getIngestionJobMock();
+      const mockMergeTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.polygonParts, OperationStatus.COMPLETED);
+      const mockInitTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.init, OperationStatus.COMPLETED);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).post('/tasks/find', { id: mockMergeTask.id }).reply(200, [mockMergeTask]);
+      nock(jobManagerConfigMock.jobManagerBaseUrl)
+        .get(`/jobs/${mockIngestionJob.id}`)
+        .query({ shouldReturnTasks: false })
+        .reply(200, mockIngestionJob);
+      nock(jobManagerConfigMock.jobManagerBaseUrl)
+        .post('/tasks/find', { jobId: mockIngestionJob.id, type: taskTypesConfigMock.init })
+        .reply(200, [mockInitTask]);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).post('/tasks/find', { jobId: mockIngestionJob.id, type: taskTypesConfigMock.finalize }).reply(404);
+      nock(jobManagerConfigMock.jobManagerBaseUrl)
+        .post(`/jobs/${mockIngestionJob.id}/tasks`, _.matches({ type: taskTypesConfigMock.finalize }))
+        .reply(201);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).put(`/jobs/${mockIngestionJob.id}`).reply(200);
+      // action
+      const response = await requestSender.handleTaskNotification(mockMergeTask.id);
+      // expectation
+      expect(response.status).toBe(200);
+      expect(response).toSatisfyApiSpec();
+    });
+
+    it('Should return 200 and fail job when getting failed task', async () => {
+      // mocks
+      const mockIngestionJob = getIngestionJobMock();
+      const mockMergeTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.polygonParts, OperationStatus.COMPLETED);
+      const mockInitTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.init, OperationStatus.COMPLETED);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).post('/tasks/find', { id: mockMergeTask.id }).reply(200, [mockMergeTask]);
+      nock(jobManagerConfigMock.jobManagerBaseUrl)
+        .get(`/jobs/${mockIngestionJob.id}`)
+        .query({ shouldReturnTasks: false })
+        .reply(200, mockIngestionJob);
+      nock(jobManagerConfigMock.jobManagerBaseUrl)
+        .post('/tasks/find', { jobId: mockIngestionJob.id, type: taskTypesConfigMock.init })
+        .reply(200, [mockInitTask]);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).post('/tasks/find', { jobId: mockIngestionJob.id, type: taskTypesConfigMock.finalize }).reply(404);
+      nock(jobManagerConfigMock.jobManagerBaseUrl)
+        .post(`/jobs/${mockIngestionJob.id}/tasks`, _.matches({ type: taskTypesConfigMock.finalize }))
+        .reply(201);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).put(`/jobs/${mockIngestionJob.id}`).reply(200);
+      // action
+      const response = await requestSender.handleTaskNotification(mockMergeTask.id);
+      // expectation
+      expect(response.status).toBe(200);
+      expect(response).toSatisfyApiSpec();
     });
   });
+
   describe('Bad Path', function () {
     // All requests with status code of 400
   });
   describe('Sad Path', function () {
     // All requests with status code 4XX-5XX
+    afterEach(() => {
+      jest.restoreAllMocks();
+      if (!nock.isDone()) {
+        throw new Error(`Not all nock interceptors were used: ${JSON.stringify(nock.pendingMocks())}`);
+      }
+      nock.cleanAll();
+    });
+
+    it('Should return 404 if the task that was notified about does not exists', async () => {
+      // mocks
+      const mockIngestionJob = getIngestionJobMock();
+      const mockMergeTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.tilesMerging, OperationStatus.COMPLETED);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).post(`/tasks/find`, { id: mockMergeTask.id }).reply(404, 'message: Tasks not found');
+      // action
+      const response = await requestSender.handleTaskNotification(mockMergeTask.id);
+      // expectation
+      expect(response.status).toBe(404);
+      expect(response).toSatisfyApiSpec();
+    });
+
+    it('Should return 428 if the task given is neither in "Completed" nor "Failed" status', async () => {
+      // mocks
+      const mockIngestionJob = getIngestionJobMock();
+      const mockMergeTask = getTaskMock(mockIngestionJob.id, taskTypesConfigMock.tilesMerging, OperationStatus.PENDING);
+      nock(jobManagerConfigMock.jobManagerBaseUrl).post('/tasks/find', { id: mockMergeTask.id }).reply(200, [mockMergeTask]);
+      // action
+      const response = await requestSender.handleTaskNotification(mockMergeTask.id);
+      // expectation
+      expect(response.status).toBe(428);
+      expect(response).toSatisfyApiSpec();
+    });
   });
 });
