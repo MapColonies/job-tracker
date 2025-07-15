8ea64a664b33fddfb12a69d8ffd485b861ca9681
BROCHSTEIN RAZ <you@example.com>
2025-07-15 16:27:12 +0300

    refactor: refactor factory's structure

--- src/tasks/handlers/jobHandlersFactory.ts
+++ src/tasks/handlers/jobHandlersFactory.ts
@@ -1,35 +1,30 @@
-import { BadRequestError } from "@map-colonies/error-types";
-import { Logger } from "@map-colonies/js-logger";
-import { IJobResponse, ITaskResponse, TaskHandler as QueueClient } from "@map-colonies/mc-priority-queue";
-import { IConfig, IJobDefinitionsConfig } from "../../common/interfaces";
-import { JobHandler } from "./baseHandler";
-import { IngestionJobHandler } from "./ingestion/ingestionHandler";
-import { ExportJobHandler } from "./export/exportHandler";
-import { ExportFinalizeJobHandler } from "./export/finalizeHandler";
+import { BadRequestError } from '@map-colonies/error-types';
+import { Logger } from '@map-colonies/js-logger';
+import { IJobResponse, ITaskResponse, TaskHandler as QueueClient } from '@map-colonies/mc-priority-queue';
+import { IConfig, IJobDefinitionsConfig } from '../../common/interfaces';
+import { JobHandler } from './baseHandler';
+import { IngestionJobHandler } from './ingestion/ingestionHandler';
+import { ExportJobHandler } from './export/exportHandler';
 
 export function initJobHandler(
-    jobHandlerType: string,
-    jobDefinitions: IJobDefinitionsConfig,
-    logger: Logger,
-    queueClient: QueueClient,
-    config: IConfig,
-    job: IJobResponse<unknown, unknown>, task: ITaskResponse<unknown>
+  jobHandlerType: string,
+  jobDefinitions: IJobDefinitionsConfig,
+  logger: Logger,
+  queueClient: QueueClient,
+  config: IConfig,
+  job: IJobResponse<unknown, unknown>,
+  task: ITaskResponse<unknown>
 ): JobHandler {
-    switch (jobHandlerType) {
-        case jobDefinitions.jobs.new:
-        case jobDefinitions.jobs.update:
-        case jobDefinitions.jobs.swapUpdate:
-            //CASESEED?
-            return new IngestionJobHandler(logger, queueClient, config, job, task)
-        case jobDefinitions.jobs.export:
-            switch (task.type) {
-                case jobDefinitions.tasks.finalize:
-                    return new ExportFinalizeJobHandler(logger, queueClient, config, job, task)
-                    break;
-            }
-            return new ExportJobHandler(logger, queueClient, config, job, task)
+  switch (jobHandlerType) {
+    case jobDefinitions.jobs.new:
+    case jobDefinitions.jobs.update:
+    case jobDefinitions.jobs.swapUpdate:
+    case jobDefinitions.jobs.seed:
+      return new IngestionJobHandler(logger, queueClient, config, job, task);
+    case jobDefinitions.jobs.export:
+      return new ExportJobHandler(logger, queueClient, config, job, task);
 
-        default:
-            throw new BadRequestError(`${jobHandlerType} job type is invalid`);
-    }
+    default:
+      throw new BadRequestError(`${jobHandlerType} job type is invalid`);
+  }
 }
