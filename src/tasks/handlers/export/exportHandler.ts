e981f1c705d06e1b38c6232f372c0e5f4e0edd8f
BROCHSTEIN RAZ <you@example.com>
2025-07-27 12:03:31 +0300

    chore: refactor handler oop another level on export

--- src/tasks/handlers/export/exportHandler.ts
+++ src/tasks/handlers/export/exportHandler.ts
@@ -1,10 +1,10 @@
 import { Logger } from '@map-colonies/js-logger';
-import { ICreateTaskBody, IJobResponse, ITaskResponse, JobManagerClient } from '@map-colonies/mc-priority-queue';
+import { IJobResponse, ITaskResponse, JobManagerClient } from '@map-colonies/mc-priority-queue';
 import { injectable, inject } from 'tsyringe';
-import { ExportFinalizeErrorCallbackParams, exportFinalizeTaskParamsSchema, ExportFinalizeType } from '@map-colonies/raster-shared';
 import { IConfig, TaskTypeArray } from '../../../common/interfaces';
 import { SERVICES } from '../../../common/constants';
 import { BaseHandler } from '../baseHandler';
+import { IValidationStrategy, FinalizeValidationStrategy, DefaultValidationStrategy } from './validationStrategies';
 
 @injectable()
 export class ExportJobHandler extends BaseHandler {
@@ -23,50 +23,28 @@ export class ExportJobHandler extends BaseHandler {
     this.tasksFlow = this.config.get<TaskTypeArray>('taskFlowManager.exportTasksFlow');
     this.excludedTypes = this.config.get<TaskTypeArray>('taskFlowManager.exportCreationExcludedTaskTypes');
     this.shouldBlockDuplicationForTypes = [this.jobDefinitions.tasks.export];
-    this.setValidations();
+
+    // Create validation strategy as a helper to set up method bindings
+    const validationStrategy = this.createValidationStrategy();
+    this.canProceed = async (): Promise<boolean> => validationStrategy.canProceed();
+    this.handleFailedTask = async (): Promise<void> => validationStrategy.handleFailedTask();
   }
 
-  // eslint-disable-next-line @typescript-eslint/require-await
-  public handleFailedTask = async (): Promise<void> => {
-    throw new Error('handleFailedTask has failed to set.');
-  };
-  public canProceed = async (): Promise<boolean> => Promise.resolve(true);
-
-  private readonly handleFailedExportTask = async (): Promise<void> => {
-    this.logger.info({ msg: `Handling export failure with jobId: ${this.task.jobId}, and reason: ${this.task.reason}` });
-    const taskParameters: ExportFinalizeErrorCallbackParams = { callbacksSent: false, type: ExportFinalizeType.Error_Callback };
-    const taskType = this.jobDefinitions.tasks.finalize;
-    const createTaskBody: ICreateTaskBody<ExportFinalizeErrorCallbackParams> = {
-      type: taskType,
-      parameters: taskParameters,
-      blockDuplication: this.shouldBlockDuplicationForTypes.includes(taskType),
-    };
-
-    await this.jobManager.createTaskForJob(this.task.jobId, createTaskBody);
-    this.logger.info({ msg: `Created ${taskType} task for job: ${this.task.jobId}` });
-    await this.updateJobForHavingNewTask(taskType);
-
-    await this.failJob();
-  };
-
-  private readonly finalizeShouldProceed = async (): Promise<boolean> => {
-    const validFinalizeTaskParams = exportFinalizeTaskParamsSchema.parse(this.task.parameters);
-    if (validFinalizeTaskParams.type === ExportFinalizeType.Error_Callback) {
-      return Promise.resolve(false);
-    }
-    return Promise.resolve(true);
-  };
-
-  private setValidations(): void {
+  private createValidationStrategy(): IValidationStrategy {
     switch (this.task.type) {
       case this.jobDefinitions.tasks.finalize:
-        this.canProceed = this.finalizeShouldProceed;
-        this.handleFailedTask = super.handleFailedTask;
-        break;
+        return new FinalizeValidationStrategy(this.task, super.canProceed.bind(this), super.handleFailedTask.bind(this));
       default:
-        this.canProceed = super.canProceed;
-        this.handleFailedTask = this.handleFailedExportTask;
-        break;
+        return new DefaultValidationStrategy(
+          this.logger,
+          this.jobManager,
+          this.task,
+          super.canProceed.bind(this),
+          this.updateJobForHavingNewTask.bind(this),
+          this.failJob.bind(this),
+          this.shouldBlockDuplicationForTypes,
+          this.jobDefinitions
+        );
     }
   }
 }
